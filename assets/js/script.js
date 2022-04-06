e77785fd5d8893766ea856211819b2f3a5f3c4e4
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-06 12:40:43 -0500

    setup api's

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Global
+//https://www.boredapi.com/documentation
+var boredApi= "http://www.boredapi.com/api/activity/"
+//https://api.chucknorris.io/#!
+var jokesApi= "https://api.chucknorris.io/jokes/random"
+
+
+function getboredactivity() {
+
+    
+    
+}
+
+
+
+function getJoke() {
+    
+
+}
