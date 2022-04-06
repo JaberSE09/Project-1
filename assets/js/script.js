4bce12f3edf51ead16820b3a90075f2633fe26a6
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-06 17:25:13 -0500

    api setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 //Global
 //https://www.boredapi.com/documentation
-var boredApi= "http://www.boredapi.com/api/activity/"
+var boredApi = "http://www.boredapi.com/api/activity/"
 //https://api.chucknorris.io/#!
-var jokesApi= "https://api.chucknorris.io/jokes/random"
+var jokesApi = "https://api.chucknorris.io/jokes/random"
 
-
+//fetch bored activity data
 function getboredactivity() {
-
-    
-    
+    fetch(boredApi)
+        .then(function (response) { return response.json() })
+        .then(function (data) { console.log(data) })
+        .catch(function (error) {console.log(error)})
 }
 
-
-
-function getJoke() {
-    
-
+function getJoke(){
+    fetch(jokesApi)
+    .then(function (response) { return response.json() })
+    .then(function (data) { console.log(data) })
+    .catch(function (error) {console.log(error)})
 }
+
+//getboredactivity()
+//getJoke()
