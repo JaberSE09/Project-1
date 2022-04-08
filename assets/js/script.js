e5de71e454180e2620dbcedc112ab8e39c1d265f
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-07 19:38:57 -0500

    start on joke api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,42 @@
 //Global
+
 //https://www.boredapi.com/documentation
 var boredApi = "http://www.boredapi.com/api/activity/"
+
 //https://api.chucknorris.io/#!
 var jokesApi = "https://api.chucknorris.io/jokes/random"
 
-//fetch bored activity data
+var jokeEl = document.getElementById("joke")
+
+
+//fetch bored activity data000
 function getboredactivity() {
     fetch(boredApi)
         .then(function (response) { return response.json() })
         .then(function (data) { console.log(data) })
-        .catch(function (error) {console.log(error)})
+        .catch(function (error) { console.log(error) })
 }
 
-function getJoke(){
+//fetch jokes data
+function getJoke() {
     fetch(jokesApi)
-    .then(function (response) { return response.json() })
-    .then(function (data) { console.log(data) })
-    .catch(function (error) {console.log(error)})
+        .then(function (response) { return response.json() })
+        .then(function (data) { 
+            var joke = data.value
+            var icon_url= data.icon_url
+            var chuckJokeImg= document.createElement("img")
+            chuckJokeImg.setAttribute("src" , icon_url)
+            jokeEl.appendChild(chuckJokeImg)
+            
+
+            
+            
+            
+            
+            
+            console.log(data) })
+        .catch(function (error) { console.log(error) })
 }
 
 //getboredactivity()
-//getJoke()
+getJoke()
