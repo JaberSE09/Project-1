c0a806a5cfb558bdf561c04b94f9e6b1c572de6f
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-04-07 21:16:22 -0500

    activity call by type functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,42 @@
 //Global
 
 //https://www.boredapi.com/documentation
-var boredApi = "http://www.boredapi.com/api/activity/"
+var boredApi = "http://www.boredapi.com/api/activity"
 
 //https://api.chucknorris.io/#!
 var jokesApi = "https://api.chucknorris.io/jokes/random"
 var activityEl = document.getElementById("calledActivity")
-
+var activityBtn = document.getElementById("newActivity")
 var jokeEl = document.getElementById("joke")
+var activityType = document.getElementById("activityType")
 
 
 //fetch bored activity data000
 function getboredactivity() {
-    fetch(boredApi)
-        .then(function (response) { return response.json() })
-        .then(function (data) {activity(data)})
-        .catch(function (error) {console.log(error)})
+    if (activityType.value === 'random') {
+        fetch(boredApi)
+            .then(function (response) { return response.json() })
+            .then(function (data) {activity(data)})
+            .catch(function (error) {console.log(error)})
+    } else {
+        var boredApiTemp = boredApi + '?type=' + activityType.value
+        fetch(boredApiTemp)
+            .then(function (response) { return response.json() })
+            .then(function (data) {activity(data)})
+            .catch(function (error) {console.log(error)})
+    }
 }
 
 //fetch jokes data
-function getJoke() {
-    fetch(jokesApi)
-        .then(function (response) { return response.json() })
-        .then(function (data) { 
-            var joke = data.value
-            var icon_url= data.icon_url
-            var chuckJokeImg= document.createElement("img")
-            chuckJokeImg.setAttribute("src" , icon_url)
-            jokeEl.appendChild(chuckJokeImg)
+// function getJoke() {
+//     fetch(jokesApi)
+//         .then(function (response) { return response.json() })
+//         .then(function (data) { 
+//             var joke = data.value
+//             var icon_url= data.icon_url
+//             var chuckJokeImg= document.createElement("img")
+//             chuckJokeImg.setAttribute("src" , icon_url)
+//             jokeEl.appendChild(chuckJokeImg)
             
 
             
@@ -35,15 +44,16 @@ function getJoke() {
             
             
             
-            console.log(data) })
-        .catch(function (error) { console.log(error) })
-}
-
+//             console.log(data) })
+//         .catch(function (error) { console.log(error) })
+// }
+// activity API call
 function activity (data) {
     console.log(data)
+    console.log(activityType.value)
     var activity = data.activity
     activityEl.innerText = activity
 }                                                 
-
+activityBtn.addEventListener("click", getboredactivity)
 getboredactivity()
 // getJoke()
