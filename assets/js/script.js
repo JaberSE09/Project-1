4621ef439cb754e6c47f864d5862915adf2034b5
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-10 12:11:26 -0500

    fixed the jokes search

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,13 @@
 var boredApi = "http://www.boredapi.com/api/activity"
 
 //https://api.chucknorris.io/#!
-var jokesApi = "https://api.chucknorris.io/jokes/random"
-
+var jokesApi = "https://api.chucknorris.io/jokes/random?category=sport"
 var jokeEl = document.getElementById("jokes")
 var jokeBtnEl = document.getElementById("start-btn")
 var activityEl = document.getElementById("calledActivity")
 var activityBtn = document.getElementById("newActivity")
 var jokeEl = document.getElementById("jokes")
 var activityType = document.getElementById("activityType")
-
 
 //fetch bored activity data000
 function getboredactivity() {
@@ -34,8 +32,33 @@ function getboredactivity() {
 function getJoke() {
     fetch(jokesApi)
         .then(function (response) { return response.json() })
-        .then(function (data) { 
+        .then(function (data) {
+
+            var jokesSearch= document.getElementById("joke").value.trim()
+            if (jokesSearch){
+            
+                var searchApi = `https://api.chucknorris.io/jokes/random?category=${jokesSearch}`
+                fetch(searchApi).then(function(response){
+                    return response.json()
+                }).then(function (data){
+                console.log(data)
+                if(data.status == 404 ){
+                    jokeEl.innerHTML=data.message
+                }
+                else if(data.categories[0] == "explicit"){
+                    jokeEl.innerHTML="no jokes found"
+                }
+                else{
+                    jokeEl.innerHTML=data.value
+                }
+                
+            })
+        }
+            else{
             jokeEl.innerHTML= data.value
+}
+
+
         })
         .catch(function (error) { console.log(error) })
 }
